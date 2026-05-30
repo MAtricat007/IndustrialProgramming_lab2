6b29bc57dea9478f300c8a574f55a133e719323b
MAtricat007 <gromashka007@gmail.com>
2026-05-30 14:41:33 +0300

    Добавлен класс Healthbar_green

---
+++ healthbar.py
@@ -0,0 +1,9 @@
+import pygame as pg
+
+class Healthbar_green(pg.sprite.Sprite):
+
+    def __init__(self, player):
+        pg.sprite.Sprite.__init__(self)
+        self.image = pg.Surface((player.health // 15, 10))
+        self.image.fill((0, 255, 0))
+        self.rect = pg.Rect(player.rect.x, player.rect.y - 12, player.health // 15, 10)
