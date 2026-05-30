1b7523577e79f8ee1e71e2bcbe9512094c7daa4f
MAtricat007 <gromashka007@gmail.com>
2026-05-30 16:19:31 +0300

    Создаю конфликт версий)

--- healthbar.py
+++ healthbar.py
@@ -18,3 +18,10 @@ class Healthbar_green(pg.sprite.Sprite):
         self.image.fill((0, 255, 0))
         self.rect = pg.Rect(player.rect.x, player.rect.y - 12, player.health // 15, 10)
 
+создаю конфликт просто так
+ысвывс
+ывсывс
+ывсывс
+ыывсывс
+ывсывс
+ывсывсы
